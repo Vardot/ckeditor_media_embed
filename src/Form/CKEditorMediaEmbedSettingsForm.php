6ab3c5ee0da8f441c496300464057fe7b07c5232
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-08-23 13:30:59 -0400

    Issue #3314733 by pieter-e1, dpi, lamp5: ModuleHandler error after upgrading Hook Event Dispatcher

--- src/Form/CKEditorMediaEmbedSettingsForm.php
+++ src/Form/CKEditorMediaEmbedSettingsForm.php
@@ -7,7 +7,7 @@ use Drupal\ckeditor_media_embed\AssetManager;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Asset\LibraryDiscoveryInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Extension\ModuleHandler;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Routing\UrlGeneratorInterface;
@@ -23,7 +23,7 @@ class CKEditorMediaEmbedSettingsForm extends ConfigFormBase {
   /**
    * The module handler.
    *
-   * @var \Drupal\Core\Extension\ModuleHandler
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
    */
   protected $moduleHandler;
 
@@ -46,7 +46,7 @@ class CKEditorMediaEmbedSettingsForm extends ConfigFormBase {
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The factory for configuration objects.
-   * @param \Drupal\Core\Extension\ModuleHandler $module_handler
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
    * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
    *   The URL generator.
@@ -54,7 +54,7 @@ class CKEditorMediaEmbedSettingsForm extends ConfigFormBase {
    *   The library discovery service to use for retrieving information about
    *   the CKeditor library.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, ModuleHandler $module_handler, UrlGeneratorInterface $url_generator, LibraryDiscoveryInterface $library_discovery) {
+  public function __construct(ConfigFactoryInterface $config_factory, ModuleHandlerInterface $module_handler, UrlGeneratorInterface $url_generator, LibraryDiscoveryInterface $library_discovery) {
     parent::__construct($config_factory);
 
     $this->urlGenerator = $url_generator;
